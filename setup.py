a745bb2cd6e07f15fd3708a00e3c4a1cb7104512
ma7350988-prog <ma7350988@gmail.com>
2026-07-16 20:00:15 +0300

    add: setup.py for better package management

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='discord-bot',
+    version='1.0',
+    packages=[],
+    install_requires=[
+        'discord.py==2.4.0',
+        'python-dotenv==1.0.1'
+    ],
+)
